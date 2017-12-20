a9dafa0165c4e1542d99e63653674831da3734cd
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2017-12-20 19:00:53 +0100

    Commons CLI

--- Aplikacja/Test CMD.bat
+++ Aplikacja/Test CMD.bat
@@ -1,2 +1,2 @@
-@echo off
-java -jar Duplikwidator.jar nogui
+@ECHO OFF
+java -jar Duplikwidator.jar -minimalnePodobienstwo 90 -metodyPorownywania pHash -szukajDla "../do testu" -szukajW "../do testu" -znajdzDuplikaty -usunDuplikaty
